aa0f04d27e09c58bc2d926d28c56ad1b180e268e
yoritin <47884591+yoritin@users.noreply.github.com>
2021-06-05 16:11:30 +0900

    edit style

--- lib/main.dart
+++ lib/main.dart
@@ -46,16 +46,17 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Center(
         child: Column(
           children: [
-            Image.network('https://images.unsplash.com/photo-1622822781732-45245667cb72?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=3150&q=80'),
-            RaisedButton(
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => NextPage('yori')),
-                );
-              },
-              child: Text('次へ')
+            Text(
+              'こんにちは',
+              style: TextStyle(
+                fontSize: 40,
+                color: Colors.green,
+                fontWeight: FontWeight.bold,
+                fontStyle: FontStyle.italic,
+                decoration: TextDecoration.lineThrough,
+              )
             ),
+            Text('かたよりです'),
           ],
         ),
       ),
