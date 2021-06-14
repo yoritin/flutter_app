737177a86b7f2181039ed47e9f3edcb8883d68d0
yoritin <47884591+yoritin@users.noreply.github.com>
2021-06-14 14:16:12 +0900

    GridViewでループする

--- lib/main.dart
+++ lib/main.dart
@@ -37,24 +37,82 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  final items = List<String>.generate(10000, (i) => "Item $i");
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: Container(
-        width: double.infinity,
-        child: ListView.builder(
-          itemCount: items.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('${items[index]}'),
-            );
-          },
-        )
+      body: GridView.count(
+        crossAxisCount: 3,
+        children: List.generate(10, (index) {
+          return Center(
+            child: Container(
+              width: 100,
+              height: 160,
+              margin: EdgeInsets.fromLTRB(10, 10, 10, 10),
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.black),
+              ),
+              child: GestureDetector(
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (context) {
+                        return SimpleDialog(
+                          children: <Widget>[
+                            SimpleDialogOption(
+                              onPressed: () => Navigator.pop(context),
+                              child: Row(
+                                  children: [
+                                    Container(
+                                      width: 180,
+                                      margin: EdgeInsets.fromLTRB(0, 0, 10, 0),
+                                      child: Column(
+                                          crossAxisAlignment: CrossAxisAlignment.start,
+                                          children: [
+                                            Container(
+                                                margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                                child: Text('基礎から学ぶFlutter', style: TextStyle(fontWeight: FontWeight.bold))
+                                            ),
+                                            Container(
+                                                margin: EdgeInsets.fromLTRB(0, 0, 0, 10),
+                                                child: Text('Flutterの本です。Flutterの本です。Flutterの本です。Flutterの本です。Flutterの本です。Flutterの本です。')
+                                            ),
+                                            RaisedButton(
+                                              child: Text('ダウンロード'),
+                                              color: Colors.green,
+                                              textColor: Colors.white,
+                                              shape: RoundedRectangleBorder(
+                                                borderRadius: BorderRadius.circular(10),
+                                              ),
+                                              onPressed: () {
+                                                // PDR Viewerを開く
+                                              },
+                                            )
+                                          ]
+                                      ),
+                                    ),
+                                    Expanded(
+                                        child: Container(
+                                            width: 80,
+                                            height: 120,
+                                            child: Image.network('https://images-na.ssl-images-amazon.com/images/I/81r-0HdedOL.jpg')
+                                        )
+                                    )
+                                  ]
+                              ),
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                  child: Image.network('https://images-na.ssl-images-amazon.com/images/I/81r-0HdedOL.jpg')
+              ),
+            ),
+          );
+        }),
       )
     );
   }
