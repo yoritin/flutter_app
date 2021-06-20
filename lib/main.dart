efc0ab131e5b4539f43bf7b22b626cbdab443964
yoritin <47884591+yoritin@users.noreply.github.com>
2021-06-20 14:44:47 +0900

    providerパターンでモデルを実装する

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/main_model.dart';
+import 'package:provider/provider.dart';
 
 import 'next_page.dart';
 
@@ -17,20 +19,29 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.grey,
       ),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('タイトルです'),
-        ),
-        body: Center(
-            child: Text(
-              '$text',
-              style: TextStyle(
-                fontSize: 30,
+      home: ChangeNotifierProvider<MainModel>(
+        create: (_) => MainModel(),
+        child: Consumer<MainModel>(
+          builder: (context, model, child) {
+            return Scaffold(
+              appBar: AppBar(
+                title: Text('タイトルです'),
               ),
-            )
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {},
+              body: Center(
+                  child: Text(
+                    model.text,
+                    style: TextStyle(
+                      fontSize: 30,
+                    ),
+                  )
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: () {
+                  model.changeText();
+                },
+              ),
+            );
+          }
         ),
       ),
     );
