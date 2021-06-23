f8f115a76d369cb2c31f7160907cbc8dbe3e552b
yoritin <47884591+yoritin@users.noreply.github.com>
2021-06-23 17:02:52 +0900

    TodoListのUIを実装する

--- lib/todo_list/todo_list.dart
+++ lib/todo_list/todo_list.dart
@@ -8,6 +8,57 @@ class TodoList extends StatelessWidget {
       appBar: AppBar(
         title: Text('Todo List')
       ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'タスク',
+                      ),
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.all(10),
+                    child: FloatingActionButton(
+                      child: Icon(Icons.add),
+                      onPressed: () {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              child: ListView(
+                children: [
+                  Card(
+                    child: CheckboxListTile(
+                      title: Text('チェックボックス'),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      value: true,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                  Card(
+                    child: CheckboxListTile(
+                      title: Text('チェックボックス'),
+                      controlAffinity: ListTileControlAffinity.leading,
+                      value: false,
+                      onChanged: (bool? value) {},
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      )
     );
   }
 }
