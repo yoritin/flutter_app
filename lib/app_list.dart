61093401c36b92cabad771e56579fd92b7fec953
yoritin <47884591+yoritin@users.noreply.github.com>
2021-06-23 21:42:27 +0900

    app listを別ファイルに切り出す

---
+++ lib/app_list.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+
+import 'counter/counter.dart';
+import 'todo_list/todo_list.dart';
+
+class AppList extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutterでいろいろなアプリを作るa'),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Container(
+              child: RaisedButton(
+                child: Text('Counter'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Counter())
+                  );
+                }
+              ),
+            ),
+            Container(
+              child: RaisedButton(
+                child: Text('Todo List'),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => TodoList())
+                  );
+                }
+              ),
+            ),
+          ]
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'app_list.dart';
 import 'counter/counter.dart';
 import 'todo_list/todo_list.dart';
 
@@ -16,46 +17,6 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.green,
       ),
       home: AppList()
-    );
-  }
-}
-
-class AppList extends StatelessWidget {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutterでいろいろなアプリを作る'),
-      ),
-      body: Center(
-        child: Column(
-          children: [
-            Container(
-              child: RaisedButton(
-                child: Text('Counter'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Counter())
-                  );
-                }
-              ),
-            ),
-            Container(
-              child: RaisedButton(
-                child: Text('Todo List'),
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => TodoList())
-                  );
-                }
-              ),
-            ),
-          ]
-        ),
-      ),
     );
   }
 }
